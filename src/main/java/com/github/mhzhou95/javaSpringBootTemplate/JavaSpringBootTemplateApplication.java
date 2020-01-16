752798619a182b344521aac9df863ce4e6257c1a
unknown <minghaozhou123@gmail.com>
2020-01-16 10:45:42 -0500

    template only. used for pulling

---
+++ src/main/java/com/github/mhzhou95/javaSpringBootTemplate/JavaSpringBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.github.mhzhou95.javaSpringBootTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootTemplateApplication.class, args);
+	}
+
+}
